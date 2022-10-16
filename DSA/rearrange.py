81f028a433542ec7d1b4b5e29c0873577a8b8ca9
Adefolarin Samson Adesoba <a.adesoba@native.semicolon.africa>
2022-10-16 01:54:20 +0100

    latest commit

--- DSA/rearrange.py
+++ DSA/rearrange.py
@@ -1,10 +1,29 @@
-def rearrange_element(lst):
-    lst1 = []
-    k = 0
-    for i in range(len(lst)):
-        lst1.append(len(lst) - k)
-        k += 1
-    return lst1
+import collections
+
+from pythonProject.DSA import linked_list
+from pythonProject.DSA.linked_list import LinkedList
 
 
-print(rearrange_element([1, 2, 3, 4, 5]))
+def rearrange_element(head : collections.deque):
+    prev = None
+    current = head
+    next = head.next
+    while current is not None:
+        next = current.next
+        # chang the direction of the nodes
+        current.next = prev
+        # shifting the nodes
+        prev = current
+        current = next
+    head = prev
+    return head
+
+
+llist = collections.deque()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(4)
+llist.append(5)
+
+print(rearrange_element(llist))
